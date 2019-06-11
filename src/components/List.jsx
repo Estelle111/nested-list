d021e60b500f627953e4417d3beba29814f8418a
Estelle <estellemol11@gmail.com>
2019-06-11 14:05:08 +0200

    list functions

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,105 +3,134 @@ import React from "react";
 class List extends React.Component {
 	constructor(props) {
 		super(props);
-		this.toggleList = this.toggleList.bind(this)
-		this.toggleButtons = this.toggleButtons.bind(this)
-		this.handleChange = this.handleChange.bind(this)
 		this.state = {
-				showList: false,
-				showButtons: false,
-				hideButton: false,
+			menuOpened: 3,
+			list: [
+				{
+					id			: 0,
+					img			: 'https://images.pexels.com/photos/1191639/pexels-photo-1191639.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260',
+					title		: 'Dunkin Donuts - FR',
+					link		: 'dunkindonuts.com/summer2017%camera',
+					size		: 'Percentage: 10%',
+					button		: '...',
+					opened  	: false,
+					list		: [
+						{
+							id		: 1,
+							img		: 'https://images.pexels.com/photos/1191639/pexels-photo-1191639.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260',
+							title	: 'FR - IMU Dunkin Donuts',
+							link	: 'dunkindonuts.com/summer2017%camera',
+							size	: '300 x 250',
+							button	: '...',
+							opened  : false,
+						}, {
+							id		: 2,
+							img		: 'https://images.pexels.com/photos/1191639/pexels-photo-1191639.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260',
+							title	: 'FR - Boutton Dunkin Donuts',
+							link	: 'dunkindonuts.com/summer2017%camera',
+							size	: '120 x 90',
+							button	: '...',
+							opened  	: false,
+						}
+					]
+				},
+				{
+					id			: 1,
+					img			: 'https://images.pexels.com/photos/1191639/pexels-photo-1191639.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260',
+					title		: 'Dunkin Donuts - FR',
+					link		: 'dunkindonuts.com/summer2017%camera',
+					size		: 'Percentage: 10%',
+					button		: '...',
+					opened  	: false,
+					list		: [
+						{
+							id		: 3,
+							img		: 'https://images.pexels.com/photos/1191639/pexels-photo-1191639.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260',
+							title	: 'FR - IMU Dunkin Donuts',
+							link	: 'dunkindonuts.com/summer2017%camera',
+							size	: '300 x 250',
+							button	: '...',
+							opened  	: false,
+						}, {
+							id		: 4,
+							img		: 'https://images.pexels.com/photos/1191639/pexels-photo-1191639.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260',
+							title	: 'FR - Boutton Dunkin Donuts',
+							link	: 'dunkindonuts.com/summer2017%camera',
+							size	: '120 x 90',
+							button	: '...',
+							opened  : false,
+						}
+					]
+				}
+			]
 		}
-		this.buttonInstance = (
-			<button onClick={this.toggleButtons}>
-				...
-				{this.state.showButtons === true ? 
-					<div className="buttons-layout">
-						<button className="buttons">delete</button>
-						<button className="buttons">edit</button>
-					</div>
-				: " "}
-			</button>
-		)
 	}
 
-	toggleList(e) {
-		e.preventDefault()
+	toggleList(id) {
+		/* console.log(id) */
+		let name = id 
+		let banners = this.state.list
+		for(let i=0; i<banners.length; i++){
+			let item = banners[i]
+			if(name === item.id){
+				item.opened = !item.opened
+			}
+		}
 		this.setState({
-			showList: !this.state.showList
+			banners: this.state.list
 		})
 	}
 
-	toggleButtons(e) {
-		e.preventDefault()
-		this.setState({
-			showButtons: !this.state.showButtons
-		})
-		console.log(e.target)
-	}
+	toggleMenu(id) {
 
-	handleChange(e) {
-		this.setState({
-			hideButton: true,
-		})
+		/* let name = id
+		let banners = this.state.list
+		for(let i=0; i<banners.length; i++){
+			let item = banners[i]
+			for(let j=0; j<item; j++){
+				let subitem = item[j]
+				console.log(id)
+			}
+			
+		} */
+		
+		
 	}
 
 	render() {
-		const list = [
-			{
-				id			: 0,
-				img			: 'https://images.pexels.com/photos/1191639/pexels-photo-1191639.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260',
-				title		: 'Dunkin Donuts - FR',
-				link		: 'dunkindonuts.com/summer2017%camera',
-				size		: 'Percentage: 10%',
-				button	: '...',
-				list		: [
-					{
-						id		: 1,
-						img		: 'https://images.pexels.com/photos/1191639/pexels-photo-1191639.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260',
-						title	: 'FR - IMU Dunkin Donuts',
-						link	: 'dunkindonuts.com/summer2017%camera',
-						size	: '300 x 250',
-						button	: '...'
-					}, {
-						id		: 2,
-						img		: 'https://images.pexels.com/photos/1191639/pexels-photo-1191639.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260',
-						title	: 'FR - Boutton Dunkin Donuts',
-						link	: 'dunkindonuts.com/summer2017%camera',
-						size	: '120 x 90',
-						button	: '...'
-					}
-				]
-			}
-		];
 
-		const {showList} = this.state
-		const {showButtons} = this.state
-		const style = this.state.hideButton ? {display: 'none'} : {};
+	 /* const {showList, showButtons} = this.state */
+	/* 	const style = this.state.hideButton ? {display: 'none'} : {}; */
 
 		return (
 			<div>
-				{list.map((item, index) => {
+				{this.state.list.map((item, index) => {
 					return (
 						<div className="list-block-layout" key={index}>
-							<div className="list-layout" onClick={this.toggleList}>
+							<div className="list-layout" onClick={() => this.toggleList(item.id)}>
 								<img src={item.img} alt="dunkin donuts"/>
 								<h3>{item.title}</h3>
 								<span>{item.link}</span>
 								<span>{item.size}</span>
-								<button>...</button>
+								<button>{item.button}</button>
 							</div>
 							{item.list.map((subitem) => {
 								return (
 									<div>
-										{showList === true ?
+										{item.opened === true ?
 											<ul class="list-layout list-children-layout">
 												<img src={subitem.img} alt="dunkin donuts"/>
 												<h3>{subitem.title}</h3>
 												<span>{subitem.link}</span>
 												<span>{subitem.size}</span>
-												<div style={style}>
-													{this.buttonInstance}
-												</div>
+												<button onClick={() => this.toggleMenu(subitem.id)}>
+													{subitem.id === this.state.menuOpened ? 
+														<div className="buttons-layout">
+															<button className="buttons">delete</button>
+															<button className="buttons">edit</button>
+														</div>
+													: <button>...</button> }
+												</button>
 											</ul>
 										: " "}
 									</div>
