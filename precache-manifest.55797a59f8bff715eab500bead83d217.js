2e70f7a262ad24988d2c0bc41434fc2a8737396b
Estelle <estellemol11@gmail.com>
2019-06-12 09:27:31 +0200

    Updates

---
+++ precache-manifest.55797a59f8bff715eab500bead83d217.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b92ef71717ebb39f03534df9f00240",
+    "url": "/nested-list/index.html"
+  },
+  {
+    "revision": "36173eba31d05f84635f",
+    "url": "/nested-list/static/css/main.c0af0f46.chunk.css"
+  },
+  {
+    "revision": "48abefb02277ad1a0a70",
+    "url": "/nested-list/static/js/2.a5aa4860.chunk.js"
+  },
+  {
+    "revision": "36173eba31d05f84635f",
+    "url": "/nested-list/static/js/main.426261f3.chunk.js"
+  },
+  {
+    "revision": "e04375186e7baa41f254",
+    "url": "/nested-list/static/js/runtime~main.9e8fc309.js"
+  }
+]);
